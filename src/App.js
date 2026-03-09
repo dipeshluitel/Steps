31950b7bf327a8f0e8a621e7802d3edba738f6b7
Dipesh <dipeshluitel40@gmail.com>
2026-03-09 22:51:18 +0545

    setting up dynamic UI behaviour

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
 
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
+  const step = 1;
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
+        <div className="active">1</div>
         <div>2</div>
         <div>3</div>
       </div>
       <p className="message">Hello</p>
 
       <div className="buttons">
-        <button>Previous</button>
-        <button>Next</button>
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>Previous</button>
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>Next</button>
       </div>
-    </div>
+    </div >
   );
 }
