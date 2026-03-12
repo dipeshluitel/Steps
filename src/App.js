ebe422367566c2cd4e7261d5bf5bca17ec8492ca
Dipesh <dipeshluitel40@gmail.com>
2026-03-12 10:22:43 +0545

    updating state based on current value

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ export default function App() {
 
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
 
   }
 
@@ -27,7 +27,7 @@ export default function App() {
 
   return (
     <>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>&times;</button>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>&times;</button>
 
       {isOpen && (
         <div className="steps">
